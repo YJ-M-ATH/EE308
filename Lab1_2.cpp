bdf2f268a2197908d603484e38a3f1b6603167d8
YJ-M-ATH <93815975+YJ-M-ATH@users.noreply.github.com>
2022-10-23 19:56:05 +0800

    Update and rename code.cpp to Lab1_2.cpp

--- Lab1_2.cpp
+++ Lab1_2.cpp
@@ -6,12 +6,10 @@
 
 using namespace std;
 
-const string KEYWORDS[] = { "auto","break","case","char","const","continue","default"
-,"do","double","else","enum","extern","float","for","goto","if"
-,"int","long","register","return","short","signed","sizeof","static"
-,"struct","switch","typedef","union","unsigned","void","volatile","while" };
+const string KEYWORDS[] = {"break","case","char","const","continue","default","do","double","else","extern","float","for","if"
+,"int","long","register","return","short","signed","sizeof","static","struct","switch","unsigned","void","while"};
 
-const int KEYWORDS_LIST_NUM = 32;
+const int KEYWORDS_LIST_NUM = 26;
 
 string path;
 int keywords_count = 0;
@@ -45,18 +43,12 @@ int main() {
 	return 0;
 }
 
-
-
 void UserInterfaceHiddenCode(string path, int level) {
 	fstream File;
 	File.open(path.c_str());
 
 	if (File.fail())
-	{
-		cout << "The file was not successfully opened"
-			<< "\n Please check that the file currently exists."
-			<< endl;
-	}
+		cout << "The file was not successfully opened \nPlease check that the file currently exists."<< endl;
 	if (level > 0 && level <= 4) {
 		CountKeywords(File);
 		if (level > 1 && level <= 4) {
@@ -72,15 +64,12 @@ void UserInterfaceHiddenCode(string path, int level) {
 	}
 }
 
-
-
 void CountKeywords(fstream& fName) {
 	string semicolon = ";";
 	string colon = ":";
 	fName.seekg(0, ios::beg);
 
-	while (fName >> str)
-	{
+	while (fName >> str){
 		words.push_back(str);
 		for (int i = 0; i < KEYWORDS_LIST_NUM; i++) {
 			if (str.compare(KEYWORDS[i]) == 0 ||
@@ -98,75 +87,57 @@ void CountKeywords(fstream& fName) {
 	cout << "total num: " << keywords_count << endl;
 }
 
-
-
 void CountSwitchAndCases() {
 	string str;
 	string target_switch = "switch";
 	string target_case = "case";
 
-	for (int i = 0; i < keywords_count; i++) {
-		if (keywordsShown[i].compare(target_switch) == 0 ||
-			(keywordsShown[i].compare(target_switch) != 0 &&
-				keywordsShown[i].substr(0, target_switch.length()).compare(target_switch) == 0 &&
-				(ispunct(keywordsShown[i][target_switch.length()]))
-				)
-			) {
+	for (int i = 0; i < keywords_count; i++)
+		if (keywordsShown[i].compare(target_switch) == 0 ||(keywordsShown[i].compare(target_switch) != 0 
+			&& keywordsShown[i].substr(0, target_switch.length()).compare(target_switch) == 0 
+				&& (ispunct(keywordsShown[i][target_switch.length()]))))
 			switchPositions.push_back(i);
-		}
-	}
+	
 	switch_count = switchPositions.size();
-	cout << "switch num: " << switch_count << endl;
-	cout << "case num: ";
+	cout << "switch num: " << switch_count << "\ncase num: "<< endl;
 	switchPositions.push_back(keywords_count);
 	switch_count = switchPositions.size();
 
-	for (int j = 0; j < switch_count - 1; j++) {
-		for (int k = switchPositions[j]; k < switchPositions[j + 1]; k++) {
+	for (int j = 0; j < switch_count-1; j++) {
+		
+		for (int k = switchPositions[j]; k < switchPositions[j + 1]; k++) 
 			if (keywordsShown[k].compare(target_case) == 0 ||
-				(keywordsShown[k].compare(target_case) != 0 &&
-					keywordsShown[k].substr(0, target_case.length()).compare(target_case) == 0 &&
-					(ispunct(keywordsShown[k][target_case.length()]))
-					)
-				) {
+			    (keywordsShown[k].compare(target_case) != 0 &&
+			     keywordsShown[k].substr(0, target_case.length()).compare(target_case) == 0 &&
+			      (ispunct(keywordsShown[k][target_case.length()])))) 
 				case_count++;
-			}
-		}
+		
 		cout << case_count << " ";
 		case_count = 0;
 	}
-	cout << endl;
 }
-
-
 
 void CountSelectionTotal() {
-	for (int i = 0; i < keywords_count; i++) {
+	for (int i = 0; i < keywords_count; i++) 
 		if (keywordsShown[i].compare("else{") == 0 || keywordsShown[i].compare("else{}") == 0 ||
-			(keywordsShown[i].compare("else") == 0 && keywordsShown[i - 1].compare("if") != 0)) {
+		     (keywordsShown[i].compare("else") == 0 && keywordsShown[i - 1].compare("if") != 0)) 
 			total_selection_count++;
-		}
-	}
 }
 
-
-
 void CountIfElseNum() {
-	const string keywordsAndBraces[] = { "auto","break","case","char","const","continue","default"
-	,"do","double","else","enum","extern","float","for","goto","if"
-	,"int","long","register","return","short","signed","sizeof","static"
-	,"struct","switch","typedef","union","unsigned","void","volatile","while","{","}" };
+	const string keywordsAndBraces[] = 
+	{"break","case","char","const","continue","default","do","double","else","extern","float","for","if"
+          ,"int","long","register","return","short","signed","sizeof","static","struct","switch","unsigned","void","while","{","}" };
 	
 	for (int i = 0; i < words.size();i++) {
-		for (int j = 0; j < KEYWORDS_LIST_NUM + 2; j++) {
+		
+		for (int j = 0; j < KEYWORDS_LIST_NUM + 2; j++) 
 			if (words[i].compare(keywordsAndBraces[j]) == 0 ||
 				(words[i] != "{" && words[i] != "}" && words[i].compare(keywordsAndBraces[j]) != 0 &&
 					words[i].substr(0, keywordsAndBraces[j].length()).compare(keywordsAndBraces[j]) == 0 &&
-					(ispunct(words[i][keywordsAndBraces[j].length()])))) {
-
+					(ispunct(words[i][keywordsAndBraces[j].length()])))) 
 				keywordsAndBracesShown.push_back(words[i]);
-			}
-		}
+		
 	}
 	
 	for (int i = 0; i < keywordsAndBracesShown.size(); i++) {
@@ -176,16 +147,12 @@ void CountIfElseNum() {
 		}
 	}
 	
-	for (int i = 0; i < keywordsAndBracesShown.size(); i++) {
-		if (keywordsAndBracesShown[i].substr(0, 2).compare("if") == 0 && keywordsAndBracesShown[i + 1].substr(0, 4).compare("else") == 0) {
-			if_else_count++;
-		}
-	}
+	for (int i = 0; i < keywordsAndBracesShown.size(); i++) 
+		if (keywordsAndBracesShown[i].substr(0, 2).compare("if") == 0 && keywordsAndBracesShown[i + 1].substr(0, 4).compare("else") == 0) 
+		        if_else_count++;
+			
 	cout << "if-else num: " << if_else_count << endl;
 }
-
-
-
 
 void CountIfElseifElseNum() {
 	if_elseif_else_count = total_selection_count - if_else_count;
